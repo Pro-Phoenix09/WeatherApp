c4d1907108f4944ff7303bad498ff141c935d133
Shivam <sikdarshivam2@gmail.com>
2025-06-17 02:42:28 +0530

    ui half done

--- weather_app/lib/weather_screen.dart
+++ weather_app/lib/weather_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatelessWidget {
@@ -20,27 +22,124 @@ class WeatherScreen extends StatelessWidget {
         ],
       ),
 
-      body: Column(
-        children: [
-          // main card
-          const Placeholder(
-            fallbackHeight: 250,
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // main card
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16)
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            '300 K',
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                      
+                          SizedBox(height: 16,),
+                      
+                          Icon(Icons.cloud, size: 64,),
+                      
+                          SizedBox(height: 16,),
+                      
+                          Text('Rain', style: TextStyle(fontSize: 18),)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),  
+              ),
+            ),
+        
+            const SizedBox(height: 20),
+        
+            Text(
+              'Weather Forecast',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
 
-          const SizedBox(height: 20),
+            const SizedBox(height: 16),
+            // weather forecast cards
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),                  
+                ],
+              ),
+            ),
+        
+            const SizedBox(height: 20),
+        
+            // additional info cards
+            const Placeholder(
+              fallbackHeight: 150,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-          // weather forecast cards
-          const Placeholder(
-            fallbackHeight: 150,
-          ),
 
-          const SizedBox(height: 20),
+class HourlyForecastItem extends StatelessWidget {
+  const HourlyForecastItem({super.key});
 
-          // additional info cards
-          const Placeholder(
-            fallbackHeight: 150,
-          ),
-        ],
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Container(
+        width: 100,
+        padding: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: const Column(
+          children: [
+            Text(
+              '03:00',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+                          
+            SizedBox(height: 8),
+                          
+            Icon(Icons.cloud, size: 32,),
+        
+            SizedBox(height: 8),
+        
+            Text(
+              '320.12'
+            ),
+          ],
+        ),
       ),
     );
   }
